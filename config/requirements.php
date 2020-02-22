81c0735b39f3f33db8748453ad4998e0f9386f8e
f122apg <f122apg@gmail.com>
2020-02-23 03:00:53 +0900

    必須モジュールのインストール確認処理を実装

--- config/requirements.php
+++ config/requirements.php
@@ -44,3 +44,10 @@ if (version_compare(INTL_ICU_VERSION, '50.1', '<')) {
 if (!extension_loaded('mbstring')) {
     trigger_error('You must enable the mbstring extension to use CakePHP.', E_USER_ERROR);
 }
+
+/*
+ * You can remove this if you are confident you have fileinfo installed.
+ */
+if (!extension_loaded('fileinfo')) {
+    trigger_error('You must enable the fileinfo extension to use CakePHP.', E_USER_ERROR);
+}
